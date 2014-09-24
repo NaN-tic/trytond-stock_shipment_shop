8b5a96abff9d84cadb7fe3fa7e90ba7b7ff13219
resteve <resteve@zikzakmedia.com>
2014-09-24 12:22:12 +0200

    pyflakes

--- shipment.py
+++ shipment.py
@@ -2,7 +2,7 @@
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import Pool, PoolMeta
+from trytond.pool import PoolMeta
 
 __all__ = ['ShipmentOut']
 __metaclass__ = PoolMeta
