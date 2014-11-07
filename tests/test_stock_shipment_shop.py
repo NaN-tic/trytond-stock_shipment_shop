1486c251219b1cb0e662adc96cc3f4f35a2afba0
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:48:33 +0100

    Cleanup tests

--- tests/test_stock_shipment_shop.py
+++ tests/test_stock_shipment_shop.py
@@ -1,38 +1,24 @@
 #!/usr/bin/env python
-#This file is part stock_shipment_shop module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part stock_shipment_shop module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 
 
 class StockShipmentShopTestCase(unittest.TestCase):
-    '''
-    Test Stock Shipment Shop module.
-    '''
+    'Test Stock Shipment Shop module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('stock_shipment_shop')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('stock_shipment_shop')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
